381bc2d177e920b2fe5b8232faf4013b078a2177
Woody <6235923@cegepmontpetit.ca>
2024-04-23 15:45:17 -0400

    Ajout de la classe CompteurHydro
    Ajout de la classe CompteurHydro qui implémente l’interface ICompteurHydro

---
+++ R21Ex (Matériel)/R21Ex/AppCompteurHydro/CompteurHydro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCompteurHydro
+{
+    public class CompteurHydro : ICompteurHydro
+    {
+		/// <summary>
+        /// stocker la consommation actuelle exprimé en kwh
+        /// </summary>
+		private int m_consommationActuelle;
+        
+		
+		public int ConsommationActuelle
+        {
+            get { return m_consommationActuelle;  }
+        }
+
+
+        public CompteurHydro()
+        {
+            m_consommationActuelle = 0;
+        }
+		/// <summary>
+        /// Mettre à jour la consommation actuelle selon la quantité consommée
+        /// </summary>
+        /// <param name="pQuantitéKwh"> nombre de kwh a ajouté</param>
+		public void Consommer(int pQuantitéKwh)
+        {
+            m_consommationActuelle += pQuantitéKwh;
+        }
+    }
+}
